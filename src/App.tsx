902fa6dd9c654d1cac0f84397ac76d1915662c1f
SteveBand <steve020629@icloud.com>
2024-08-01 00:07:38 +0300

    dashboard hero added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles/main.scss";
-import { Route, Router, Routes } from "react-router-dom";
-import Dashboard from "./views/dashboard";
+import { Route, Routes } from "react-router-dom";
+import { Dashboard } from "./views/dashboard";
 import Header from "./components/Header";
 
 function App() {
--- src/views/dashboard.tsx
+++ src/views/dashboard.tsx
@@ -1,5 +1,43 @@
-import React from "react";
+import { useRef } from "react";
+import { TypeAnimation } from "react-type-animation";
+import { FiArrowUpRight } from "react-icons/fi";
 
-export default function Dashboard(): JSX.Element {
-  return <></>;
-}
+export const Dashboard: React.FC = () => {
+  const textEl = useRef<HTMLSpanElement>(null);
+
+  return (
+    <section className="hero-section">
+      <article className="hero-title">
+        <h1>
+          We develop
+          <br />
+          <span className="colored">amazing </span>
+          <TypeAnimation
+            sequence={["websites", 4000, "apps", 4000]}
+            wrapper="span"
+            speed={1}
+            repeat={Infinity}
+            deletionSpeed={1}
+          />{" "}
+          <br />
+          for your business
+        </h1>
+
+        <p className="desc">
+          Discover our seamless workflow as we transform Figma designs into
+          stunning <br />
+          Webflow websites. Explore the synergy between design and development
+          for <br />a pixel-perfect online experience.
+        </p>
+
+        <div className="hero-bottom">
+          <button>
+            View work <FiArrowUpRight />
+          </button>
+
+          <div></div>
+        </div>
+      </article>
+    </section>
+  );
+};
