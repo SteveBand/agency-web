139a715837224570ae465e00cff0b8d324ef47a6
SteveBand <steve020629@icloud.com>
2024-08-03 14:21:11 +0300

    added glowing background to the corners

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,24 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { FiArrowUpRight } from "react-icons/fi";
 import { BsList } from "react-icons/bs";
+import { useEffect } from "react";
 
 export default function Header() {
+  useEffect(() => {
+    const header = document.getElementsByTagName("header")[0];
+    const scroll = window.scrollY;
+    function scrollFunc() {
+      const scroll = window.scrollY;
+      if (scroll > 80) {
+        header.classList.add("active-header");
+      } else {
+        header.classList.remove("active-header");
+      }
+    }
+
+    window.addEventListener("scroll", scrollFunc);
+    return () => window.removeEventListener("scroll", scrollFunc);
+  }, []);
   return (
     <header>
       <nav>
